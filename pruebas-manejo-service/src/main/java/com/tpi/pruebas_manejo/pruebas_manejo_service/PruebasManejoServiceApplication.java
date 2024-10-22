908ebc87dee06561fd96a38bcfcb282db5d0ac51
Santiago Almada <santialmada2003@gmail.com>
2024-10-22 18:27:05 -0300

    agregados los proyectos gateway y pruebas-manejo-service

---
+++ pruebas-manejo-service/src/main/java/com/tpi/pruebas_manejo/pruebas_manejo_service/PruebasManejoServiceApplication.java
@@ -0,0 +1,13 @@
+package com.tpi.pruebas_manejo.pruebas_manejo_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PruebasManejoServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PruebasManejoServiceApplication.class, args);
+	}
+
+}
---
+++ pruebas-manejo-service/src/test/java/com/tpi/pruebas_manejo/pruebas_manejo_service/PruebasManejoServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tpi.pruebas_manejo.pruebas_manejo_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PruebasManejoServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
