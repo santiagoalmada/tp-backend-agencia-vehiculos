7dd5cedf1b8c9e3468f0c3cbbd6d1c53c77644ef
Santiago Almada <santialmada2003@gmail.com>
2024-11-07 23:31:33 -0300

    creado notificacion service 2

---
+++ notificaciones-service/src/main/java/utn/frc/backend/tutor/tp/NotificacionesApplication.java
@@ -0,0 +1,13 @@
+package utn.frc.backend.tutor.tp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotificacionesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotificacionesApplication.class, args);
+	}
+
+}
---
+++ notificaciones-service/src/test/java/utn/frc/backend/tutor/tp/NotificacionesApplicationTests.java
@@ -0,0 +1,13 @@
+package utn.frc.backend.tutor.tp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificacionesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
